66e37faa727531d9633bd18f01709fc08fa278ed
KutiLevente <kuti.levi@gmail.com>
2025-03-03 08:42:03 +0000

    restconf 2025.03.03

---
+++ c8000-get.py
@@ -0,0 +1,20 @@
+import requests, json
+
+a=("admin", "admin")
+url=("https://172.19.255.216:443/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1")
+h={"Accept": "application/yang-data+json"}
+
+#try:
+reply = requests.get(url,headers=h,auth=a, verify = False)
+
+#except:
+#	print("Hiba")
+#	exit
+
+if reply.status_code == 200:
+	print(reply.json())
+else:
+	print(reply.text)
+
+print(reply.status_code)
+
